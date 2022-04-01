29a74ef77a08de554e3932ef8cb8d5e04cf1836f
KatarinaRS <katarina.roser@gmail.com>
2022-04-01 22:27:03 +0200

    String variables

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // only change code below this line
+    var myFirstName = "Katarina";
+    var myLastName = "Roser Salihovic";
+    var myHomeTown = "Celje";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
